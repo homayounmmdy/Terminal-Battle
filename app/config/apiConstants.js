f02da8d180a404291476436e75a37c4ef8002e6f
Homayoun <homayoun763@gmail.com>
2025-07-24 12:16:29 +0330

    multiplesOf3Or5

--- app/config/apiConstants.js
+++
@@ -1,5 +0,0 @@
-export const POST_API_URL = "/api/posts";
-export const PUBLICATIONS_API_URL = "/api/publications";
-export const SECTIONS_API_URL = "/api/sections";
-export const SERVICES_API_URL = "/api/services";
-export const CONTACTS_API_URL = "/api/contacts";
